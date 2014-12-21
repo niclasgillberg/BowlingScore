2d6da69bbb079d4e91c5b51fdc9bbb0c5f812dae
Niclas Sahlin <niclas@niclassahlin.com>
2014-12-21 22:11:30 +0100

    Started with the web part

---
+++ src/BowlingScore.Web/Scripts/BowlingScore.js
@@ -0,0 +1,8 @@
+/// <reference path="./typings/jquery/jquery.d.ts"/>
+var BowlingScore;
+(function (BowlingScore) {
+    $(function () {
+        console.log("Application started");
+    });
+})(BowlingScore || (BowlingScore = {}));
+//# sourceMappingURL=BowlingScore.js.map
---
+++ src/BowlingScore.Web/Scripts/BowlingScore.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="./typings/jquery/jquery.d.ts"/>
+
+module BowlingScore {
+  $(() => {
+    console.log("Application started");
+  });
+}
